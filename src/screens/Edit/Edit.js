27d8f3d297acbc687ba44e1288d41a98090f9706
Drzaln <drzaln@null.net>
2021-01-31 12:03:15 +0700

    progress edit screen

---
+++ src/screens/Edit/Edit.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+const Edit = () => {
+    return (
+        <View>
+            <Text>Edit</Text>
+        </View>
+    )
+}
+
+export default Edit
